c3ae896af81662bd46442d3f04f9dd7c04571cc4
이정배 <jblee@bankbe.net>
2022-07-25 17:45:12 +0900

    Add: 결제요청, 취소 페이지 UI 제작

--- src/pages/PaymentCancel.js
+++ src/pages/PaymentCancel.js
@@ -1,11 +1,67 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Box, Button, styled, TextField} from "@mui/material";
+import axios from "axios";
 
 const PaymentCancel = () => {
+    const [paymentCancelData, setPaymentCancelData] = useState({
+        pgcode : "mobile",
+        client_id:"pay_test",
+        user_id:"test_user_id",
+        tid:"tpay_test2018010123595900001",
+        amount : "",
+        ip_addr:"",
+    })
+    const {pgcode, client_id, user_id, tid, amount, ip_addr} = paymentCancelData;
+
+
+    const handleChange = (e) => {
+        let {value, name} = e.target
+
+        if(name == "amount" ){
+            value = Number(value)
+        }
+
+        setPaymentCancelData({
+            ...paymentCancelData, // 기존의 input 객체를 복사한 뒤
+            [name]: value // name 키를 가진 값을 value 로 설정
+        });
+
+    };
+
+    const onClickHandler = (e) => {
+        e.preventDefault()
+        axios.post('v1.0/payments/request', paymentCancelData)
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    console.log(paymentCancelData)
+
+
     return (
-        <div>
-            결제취소
-        </div>
+        <Box
+            component="form"
+            autoComplete="off"
+            sx={{display:'flex',flexDirection:'column',margin:1}}
+        >
+            <StyledTextField label="pgcode" variant="outlined" name="pgcode" value={pgcode} onChange={handleChange} />
+            <StyledTextField label="client_id" variant="outlined" name="client_id" value={client_id} onChange={handleChange}/>
+            <StyledTextField label="user_id" variant="outlined" name="user_id" value={user_id} onChange={handleChange}/>
+            <StyledTextField label="tid" variant="outlined" name="tid" value={tid} onChange={handleChange}/>
+            <StyledTextField label="amount" variant="outlined" name="amount" value={amount} onChange={handleChange} />
+            <StyledTextField label="ip_addr" variant="outlined" name="ip_addr" value={ip_addr} onChange={handleChange} />
+            <Button type="submit" style={{width:300, margin:17}} variant="contained" onClick={onClickHandler}>전송</Button>
+        </Box>
     );
 };
 
 export default PaymentCancel;
+
+const StyledTextField = styled(TextField)({
+    margin:"16px" ,
+    width:300
+})
--- src/pages/PaymentResquest.js
+++ src/pages/PaymentResquest.js
@@ -1,11 +1,226 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {
+    Box,
+    TextField,
+    InputLabel,
+    MenuItem,
+    FormControl,
+    ListItemText,
+    Select,
+    Checkbox,
+    OutlinedInput, Button, styled
+} from "@mui/material";
+import axios from "axios";
+
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+    PaperProps: {
+        style: {
+            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+            width: 250,
+        },
+    },
+};
+
+const paymentWay = [
+    {
+        title: "신용카드",
+        value: "creditcard"
+    },
+    {
+        title: "인터넷뱅킹(금융결제원)",
+        value: "banktransfer"
+    },
+    {
+        title: "가상계좌",
+        value: "virtualaccount"
+    },
+    {
+        title: "휴대폰",
+        value: "mobile"
+    },
+    {
+        title: "도서문화상품권",
+        value: "book"
+    },
+    {
+        title: "문화상품권",
+        value: "culture"
+    },
+    {
+        title: "스마트문상",
+        value: "smartculture"
+    },
+    {
+        title: "해피머니상품권",
+        value: "happymoney"
+    },
+    {
+        title: "모바일팝",
+        value: "mobilepop"
+    },
+    {
+        title: "틴캐시",
+        value: "teencash"
+    },
+    {
+        title: "교통카드결제",
+        value: "tmoney"
+    },
+    {
+        title: "편의점캐시",
+        value: "cvs"
+    },
+    {
+        title: "에그머니",
+        value: "eggmoney"
+    },
+    {
+        title: "온캐시",
+        value: "oncash"
+    },
+    {
+        title: "폰빌",
+        value: "phonebill"
+    },
+    {
+        title: "캐시비",
+        value: "cashbee"
+    },
+    {
+        title: "카카오페이",
+        value: "kakaopay"
+    },
+    {
+        title: "페이코",
+        value: "payco"
+    },
+    {
+        title: "체크페이",
+        value: "checkpay"
+    },
+    {
+        title: "토스",
+        value: "toss"
+    },
+    {
+        title: "SSG페이",
+        value: "ssgpay"
+    },
+    {
+        title: "L.Pay",
+        value: "lpay"
+    },
+    {
+        title: "네이버페이",
+        value: "naverpay"
+    },
+    {
+        title: "삼성페이",
+        value: "samsungpay"
+    },
+    {
+        title: "차이",
+        value: "chai"
+    },
+    {
+        title: "스마일페이",
+        value: "smailpay"
+    },
+];
 
 const PaymentRequest = () => {
+    const [paymentRequestDatas, setPaymentRequestDatas] = useState({
+        pgcode: "",
+        user_id: "test_user_id",
+        user_name: "테스터",
+        sercive_name:"페이레터",
+        client_id: "pay_test",
+        amount: "",
+        taxfree_amount: "",
+        tax_amount: "",
+        product_name: "테스트상품",
+        order_no:"1234567890",
+        return_url:"https://testpg.payletter.com/result",
+        callback_url:"https://testpg.payletter.com/callback",
+        cancel_url:"https://testpg.payletter.com/cancel"
+    });
+
+    const {pgcode, user_id, user_name,sercive_name ,client_id ,amount, taxfree_amount, tax_amount, product_name, } = paymentRequestDatas;
+
+
+    const handleChange = (e) => {
+        let {value, name} = e.target
+
+        if(name == "amount" || name == "taxfree_amount" || name == "tax_amount"){
+            value = Number(value)
+        }
+
+        setPaymentRequestDatas({
+            ...paymentRequestDatas, // 기존의 input 객체를 복사한 뒤
+            [name]: value // name 키를 가진 값을 value 로 설정
+        });
+
+    };
+
+    const onClickHandler = (e) => {
+        e.preventDefault()
+        axios.post('v1.0/payments/request', paymentRequestDatas)
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    console.log(paymentRequestDatas)
+
+
     return (
-        <div>
-            요청
-        </div>
+        <Box
+            component="form"
+            autoComplete="off"
+            sx={{display:'flex',flexDirection:'column',margin:1}}
+        >
+            <FormControl sx={{margin:2 ,width: 300}}>
+                <InputLabel>결제수단</InputLabel>
+                <Select
+                    value={pgcode}
+                    onChange={handleChange}
+                    name="pgcode"
+                    input={<OutlinedInput label="결제수단"/>}
+                    MenuProps={MenuProps}
+                >
+                    {paymentWay.map((way) => (
+                        <MenuItem key={way.title} value={way.value}>
+                            <ListItemText>
+                                {way.title}
+                            </ListItemText>
+                        </MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+            <StyledTextField label="user_name" variant="outlined" name="user_name" value={user_name} onChange={handleChange} />
+            <StyledTextField label="user_id" variant="outlined" name="user_id" value={user_id} onChange={handleChange} />
+            <StyledTextField label="sercive_name" variant="outlined" name="sercive_name" value={sercive_name} onChange={handleChange} />
+            <StyledTextField label="client_id" variant="outlined" name="client_id" value={client_id} onChange={handleChange} />
+            <StyledTextField label="amount" variant="outlined" name="amount" value={amount} onChange={handleChange} />
+            <StyledTextField label="taxfree_amount" variant="outlined" name="taxfree_amount" value={taxfree_amount} onChange={handleChange} />
+            <StyledTextField label="tax_amount" variant="outlined" name="tax_amount" value={tax_amount} onChange={handleChange} />
+            <StyledTextField label="product_name" variant="outlined" name="product_name" value={product_name} onChange={handleChange} />
+
+            <Button type="submit" style={{width:300, margin:17}} variant="contained" onClick={onClickHandler}>전송</Button>
+
+        </Box>
     );
 };
 
 export default PaymentRequest;
+
+const StyledTextField = styled(TextField)({
+    margin:"16px" ,
+    width:300
+})
